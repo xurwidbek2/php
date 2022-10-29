19609335db11219e75422b9b8e645a986bd992df
xurwidbek2 <xurshidoktamov777@gmail.com>
2022-10-29 16:32:02 +0800

    my new site

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php
+$full_name = "Khurshidbek";
+$birthday = "22 april";
+$age = "22";
+$email = "xurshidoktamov7@gmail.com";
+$city = "Fergana, UZB";
+$website = "No!";
+$degree = "Junior";
+$phone = "+8613053200607";
+?>
